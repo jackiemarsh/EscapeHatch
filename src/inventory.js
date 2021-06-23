1b33b08c56a508cda150aadd84b9ba69bdf5161c
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-23 11:47:56 -1000

    Add row looping to inventory box

--- src/inventory.js
+++ src/inventory.js
@@ -59,17 +59,17 @@ export default class Inventory {
     //   let inventory = new Inventory(120, 8, 128, 32);
     generateAllItems() {
         let generatedItems = [
-             new Item("barrel", 0, 0, 1000, 800, Math.random() * 250, Math.random() * 120, 75, 75, "../dist/assets/inventory/barrel.png"),
-             new Item("bread", 0, 0, 466, 319, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/bread.png"),
-             new Item("coin", 0, 0, 345, 417, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/coin.png"),
-            //  new Item("crystal", 0, 0, 361, 538, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/crystal.png"),
-            //  new Item("shell", 50, 0, 50, 50, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/shell.png"),
-            //  new Item("key", 32, 0, 32, 32, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/KeyIcons.png"),
-            //  new Item("letters", 0, 0, 581, 458, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/paper.png"),
-            //  new Item("bottle", 0, 0, 400, 500, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/bottle.png"),
-            //  new Item("quiche", 0, 0, 290, 290, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/quiche.png"),
-            //  new Item("skull", 0, 0, 346, 398, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/skull.png"),
-            //  new Item("wood", 0, 0, 514, 423, Math.random() * 240, Math.random() * 240, 75, 75, "../dist/assets/inventory/wood.png"),
+            //  new Item("barrel", 50, 200, 950, 600, Math.random() * 250, Math.random() * 120, 80, 51, "../dist/assets/inventory/barrel.png"),
+             new Item("bread", 0, 0, 466, 319, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/bread.png"),
+             new Item("coin", 0, 0, 345, 417, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/coin.png"),
+             new Item("crystal", 0, 0, 361, 538, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/crystal.png"),
+             new Item("shell", 100, 0, 50, 50, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/shell.png"),
+             new Item("key", 32, 0, 32, 32, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/KeyIcons.png"),
+             new Item("letters", 0, 0, 581, 458, Math.random() * 240, Math.random() * 240, 45, 45, "../dist/assets/inventory/paper.png"),
+             new Item("bottle", 0, 0, 400, 500, Math.random() * 240, Math.random() * 240, 45, 45, "../dist/assets/inventory/bottle.png"),
+             new Item("quiche", 0, 0, 290, 290, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/quiche.png"),
+             new Item("skull", 0, 0, 346, 398, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/skull.png"),
+             new Item("wood", 0, 0, 514, 423, Math.random() * 240, Math.random() * 240, 35, 35, "../dist/assets/inventory/wood.png"),
             ];
         return generatedItems;
     }
@@ -79,12 +79,17 @@ export default class Inventory {
 
         if (this.inventoryItems.length) { 
             this.inventoryItems.forEach(item => {
-                console.log(item)
+                // console.log(item)
                 let index = this.inventoryItems.indexOf(item);
+                let i = (this.w / this.columns * (index % this.columns))
+                let jdex = 0
+                    if (index > 5) {
+                        jdex = 1
+                    }
                 let newItem = new Image();
                 newItem.src = item.src;
                 newItem.onload = () => { 
-                this.ctx.drawImage(newItem, item.x, item.y, item.w, item.h, (this.w / this.columns * index), 55, 35, 35) 
+                this.ctx.drawImage(newItem, item.x, item.y, item.w, item.h, i, (this.h / this.rows * jdex)+15 , item.dw, item.dh) 
                 }
             });
         }
@@ -110,20 +115,18 @@ export default class Inventory {
         this.ctx.fillStyle = "lightgray";
         this.ctx.fillRect(this.x, this.y, this.w, this.h);
 
-        // if (this.inventoryItems.length) { 
-        //     for (let lastIndex = this.items.length-1; index > -1; --index) {
-        //         let item = this.items[lastIndex];
-        //         let d_x = this.w / this.columns * lastIndex;
-        //         // let d_y = this.y / this.rows * lastIndex;
-                
-        //         this.ctx.drawImage();
-        //         // context.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, context.canvas.width, context.canvas.height);
-        //     };
-        // }
+        const coin = new Image();
+        coin.src = "../dist/assets/inventory/coin_gold.png"
+        for (let i = 0; i<this.canvas.width; i+=50) {
+            for (let j = 0; j<this.canvas.clientHeight; j+=30) {
+                coin.onload = () => { 
+                this.ctx.drawImage(coin, 0, 0, 32, 32, i, j, 25, 25)
+                }
+            }
+        }
     };
 
     drawAllItems() {
-         
         for (let index = this.items.length-1; index > -1; --index) {
             let item = items[index];
 
